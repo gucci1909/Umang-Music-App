5d6f29b25b1db4031505dfd207909692909a9909
gucci1909 <umangarora0134@gmail.com>
2022-12-09 02:14:50 +0530

    portfolio

--- my-music-app/src/Pages/Email.jsx
+++ my-music-app/src/Pages/Email.jsx
@@ -20,6 +20,7 @@ function Email() {
   const [success, setSuccess] = useState(false);
   const [loading, setLoading] = useState(false);
   const [isError, setIsError] = useState(false);
+  console.log(isError);
   const navigate = useNavigate();
   const handleChange = (e) => {
     let { name, value } = e.target;
